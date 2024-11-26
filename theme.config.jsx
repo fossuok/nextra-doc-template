ce5b3ff0e778eef43caf96474dac7abcccefa391
PinsaraPerera <1pawanpinsara@gmail.com>
2024-11-26 14:43:05 +0530

    added tailwind configurations

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>FOSSUOK design Guidelines</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/fossuok'
     }
     // ... other theme options
   }
