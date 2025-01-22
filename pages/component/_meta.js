47c199f99de6350e35abe49a3192a67afb8f4c4f
UdasriHasindu <usrimart@gmail.com>
2025-01-22 07:53:05 +0530

    add form

--- pages/component/_meta.js
+++ pages/component/_meta.js
@@ -3,6 +3,7 @@ const meta = {
     "alerts": "Alert",
     "button": "Button",
     "card": "Card",
+    "form": "Form",
     "form-inputs": "Form Inputs",
     "modal": "Modal",
     "navigation": "Navigation",
